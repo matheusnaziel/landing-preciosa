a1523f43b4781e9ee36a4a8c74cd3654e32d3337
matheusnaziel <matheus.naziel@gmail.com>
2023-12-08 00:39:20 -0300

    ajustando hero

--- components/hero.js
+++ components/hero.js
@@ -26,7 +26,7 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center w-full lg:w-1/2">
+        <div className="hidden md:flex items-center justify-center w-full lg:w-1/2 ">
           <div className="">
             <Image
               src={heroImg}
