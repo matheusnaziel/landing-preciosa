a42eebbf5dc99893f633a04e557cfb5ceb9b8577
matheusnaziel <matheus.naziel@gmail.com>
2023-12-12 13:41:36 -0300

    alterando link de font

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
-          <Link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap"
-            rel="stylesheet"
-          />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
