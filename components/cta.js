b94b9d61b16d0e73602d3ed9cc578c6bc779b333
matheusnaziel <matheus.naziel@gmail.com>
2024-02-27 11:45:07 -0300

    alterando call to action

--- components/cta.js
+++ components/cta.js
@@ -6,8 +6,8 @@ const Cta = () => {
     <Container>
       <div id="contato" className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-blue-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
         <div className="flex-grow text-center lg:text-left">
-          <h2 className="text-2xl font-medium lg:text-3xl">
-            Venha consumir água mineral natural com extrema qualidade 💎
+          <h2 className="text-3xl font-medium lg:text-3xl">
+            Entre em contato para Água Mineral Preciosa e Bala Juquinha 💎
           </h2>
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
